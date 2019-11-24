d33a43da132274c2b665bfd7c53f99fe7032bee7
Francesco Proietti <franciskittu@gmail.com>
2019-11-24 10:28:07 +0100

    Update index.pl
    provare il webhook

--- index.pl
+++ index.pl
@@ -10,6 +10,6 @@ set environment => 'production';
 
 my $psgi;
 $psgi = path($ENV{'DOCUMENT_ROOT'}, 'bin', 'app.psgi');
-die "Unable to read startup script: $psgi" unless -r $psgi;
+die "Unable to read startup scripts: $psgi" unless -r $psgi;
 
 Plack::Runner->run($psgi);
